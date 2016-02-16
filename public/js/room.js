59b0028bbb9a1cf6def3486b9b9175eb4a23e6a1
ManiacMike <mikewantacareer@gmail.com>
2016-02-16 22:58:40 +0800

    untested

--- public/js/room.js
+++ public/js/room.js
@@ -11,24 +11,8 @@ $(function(){
         },"json");
       }
   });
-  $("#my_cards").on("click","div",function(){
-    var id = $(this).children("img").attr("data-id")
-    selected_my_card = id
-    console.log(selected_my_card);
-  });
-  $("#keyword_btn").click(function(){
-    if(selected_my_card == 0){
-      alert("请选择一个手牌")
-    }else {
-      var keyword = $("#keyword_input").val()
-      if(keyword != ""){
-        ws.send('{"type":"hostpick","card":'+selected_my_card+',"keyword":\"'+keyword+'\"}');
-        $("#keyword_ui").fadeOut()
-      }
-    }
-  });
+
   var status = false;
-  var connected_ulist;
   ws.onopen = function() {
     status = true
   };
@@ -45,14 +29,64 @@ $(function(){
     }else if(msg.type == "user_disconnect"){
       updateUserList(msg.user_list)
     }else if(msg.type == "start"){
-      html = "";
-      for(var i=0;i<msg.cards.length;i++){
-        html += "<div class=\"col-md-2 my_card\"><img data-id=\""+msg.cards[i]+"\" src=\"/images/"+msg.cards[i]+".jpg\"/></div>";
-      }
+      my_cards = msg.cards
       $("#start_ui").hide();
+      $("#user_list").hide();
+      $("#host_name").html(msg.host)
+      $("#score_borad_ui").show()
+      updateScoreBoard()
+      var html = getCardsHtml(msg.cards)
       $("#my_cards").html(html)
       if(msg.host == uid){
-        $("#keyword_ui").fadeIn()
+        showHostOption()
+      }
+      host = msg.host
+    }else if(msg.type == "hostpick"){
+      $("#keyword_show_ui").fadeIn();
+      $("#keyword_show_ui").find("h2").html(msg.keyword)
+      if(host != uid){
+        $("#my_cards").on("click","div",function(){
+          var id = $(this).children("img").attr("data-id")
+          selected_my_card = id
+          var c = confirm("确定选择这张牌吗")
+          if (c == true){
+            $(this).fadeOut()
+            removeCard(selected_my_card)
+            var html = getCardsHtml(my_cards)
+            $("#my_cards").html(html)
+            ws.send('{"type":"guestpick","card":'+selected_my_card+'}')
+          }
+        });
+      }
+    }else if(msg.type == "guestpick"){
+      var html = getCardsHtml(msg.cards)
+      $("#table_cards").html(html)
+      if(host != uid){
+      $("#table_cards").on("click","div",function(){
+        var id = $(this).children("img").attr("data-id")
+        if(selected_my_card != id){
+          selected_guess_card = id
+          var c = confirm("确定这张牌是正确的牌吗")
+          if (c == true){
+            ws.send('{"type":"guess","card":'+selected_guess_card+'}')
+          }
+        }
+      });
+      }
+    }else if(msg.type == "result"){
+      if(msg.gameover == true){
+        alert("gameover")
+      }else{
+        updateScoreBoard(msg.score)
+        $("#round").html(msg.round)
+        $("#host_name").html(msg.host)
+        my_cards.push(msg.fillcard)
+        var html = getCardsHtml(my_cards)
+        $("#my_cards").html(html)
+        host = msg.host
+        if(msg.host == uid){
+          showHostOption()
+        }
       }
     }
   };
@@ -71,5 +105,58 @@ $(function(){
     }
     htmlStr+="</div>"
     $("#user_list").html(htmlStr)
+  }
+
+  function updateScoreBoard(scores){
+    var html = "",score;
+    for(var i=0;i<connected_ulist.length;i++){
+      var uid = connected_ulist[i]
+      if(typeof(scores) == "undefined"){
+        score = 0
+      }else{
+        score = scores[uid]
+      }
+      html += "<div class=\"col-md-2\">"+uid+"("+score+")</div>"
+    }
+    $("#score_board_content").html(html)
+  }
+
+  function getCardsHtml(cards){
+    var html = "";
+    for(var i=0;i<cards.length;i++){
+      html += "<div class=\"col-md-2\"><img data-id=\""+msg.cards[i]+"\" src=\"/images/"+msg.cards[i]+".jpg\"/></div>";
+    }
+    return html
+  }
+
+  function removeCard(card){
+    for (var i = 0; i < my_cards.length; i++) {
+      if(my_cards[i] == card){
+
+      }
+    }
+  }
+
+  function showHostOption(){
+    $("#keyword_ui").fadeIn()
+    $("#keyword_btn").click(function(){
+      if(selected_answer_card == 0){
+        alert("请选择一个手牌")
+      }else {
+        var keyword = $("#keyword_input").val()
+        if(keyword != ""){
+          ws.send('{"type":"hostpick","card":'+selected_answer_card+',"keyword":\"'+keyword+'\"}');
+          $("#keyword_ui").fadeOut()
+          removeCard(selected_answer_card)
+          var html = getCardsHtml(my_cards)
+          $("#my_cards").html(html)
+        }
+      }
+    });
+    $("#my_cards").on("click","div",function(){
+      var id = $(this).children("img").attr("data-id")
+      selected_answer_card = id
+      console.log(selected_answer_card);
+    });
   }
 });
